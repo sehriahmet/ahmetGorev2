e44a8d6da2b78b709487b7302658715ff5407cc8
sehriahmet <sehriyarogluahmet@gmail.com>
2024-08-11 01:42:54 +0300

    feat(run-python-code): add shell arg in php

--- save.php
+++ save.php
@@ -4,7 +4,11 @@ $content = $_POST['content'];
 $file = ('test.html');
 
 if (file_put_contents($file, $content)) {
-    echo "Content saved";
+    echo "Content saved<br>";
+
+    $output = shell_exec("python3 add-html.py 2>&1");
+
+    echo "<pre>$output</pre>";
 } else {
     echo "Error occured";
 }
